1f4996b250fba4c5227f5998b4365efbf82f46c0
卫正明 <weizhengming@xiaomi.com>
2022-03-14 15:14:22 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.type = function () {
+        console.log(123);
+    };
+    return Person;
+}());
+var B = /** @class */ (function (_super) {
+    __extends(B, _super);
+    function B() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return B;
+}(Person));
+var a = new B();
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+class Person{
+    static type = ()=>{
+        console.log(123)
+    }
+}
+
+class  B extends Person{
+
+}
+const a = new B()
